c6d5889735cd0b10aeca1262f758f341110417ac
Michael Osarodion <macmive@gmail.com>
2020-08-18 13:58:22 +0100

    generate a set of lottery numbers added

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
-# This is a sample Lottery game.
-# User can pick 6 numbers
 # Lottery calculates 6 random numbers between 1 & 20
 # Match users numbers to lottery numbers
 # Calculate users winnings based on how many numbers were matched correctly
+import random
 
 
+# get six numbers from user
 def get_player_numbers():
     input_csv = input("Enter 6 numbers separated by commas: ")
     number_list = input_csv.split(",")
 
+    # number_set = set()
+    # for number in number_list:
+    #     number_set.add(int(number))
+    # return number_set
+
     return {int(number) for number in number_list}
+
+
+# generates 6 random numbers in a set between 1 & 20
+def create_lottery_numbers():
+
+    lottery_set = set()
+    while len(lottery_set) != 6:
+        lottery_set.add(random.randint(1, 20))
+
+    return lottery_set
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print(get_player_numbers())
+    print(create_lottery_numbers())
 
